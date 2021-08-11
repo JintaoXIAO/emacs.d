b8ab542cd1b30fe4a7deeb0419f24732de5b46ee
jintao_xiao <xiaojintao@outlook.com>
2021-08-11 21:41:49 +0800

    add init config

---
+++ lisp/init-local.el
@@ -0,0 +1,28 @@
+(provide 'init-local)
+
+;; set default font
+(when (member "PT Mono" (font-family-list))
+  (set-frame-font "PT Mono-14" t t))
+
+;; set default window size
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+            '(
+              (tool-bar-lines . 0)
+              (width . 106)             ; chars
+              (height . 60)             ; lines
+              ;;              (background-color . "honeydew")
+              (left . 50)
+              (top . 50)))
+      (setq default-frame-alist
+            '(
+              (tool-bar-lines . 0)
+              (width . 106)
+              (height . 60)
+              ;;            (background-color . "honeydew")
+              (left . 50)
+              (top . 50))))
+  (progn
+    (setq initial-frame-alist '( (tool-bar-lines . 0)))
+    (setq default-frame-alist '( (tool-bar-lines . 0)))))
